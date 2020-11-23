788d04aa00a12254bcd9666d8159378ffbbcb794
my3154 <74883294+my3154@users.noreply.github.com>
2020-11-23 21:33:50 +0800

    关于if先后顺序的一点小意外
    只是单单的改变了if语句的顺序，就使得输出有了改变。
    如在“迷惑”中输入4，会出现X10 10，而非想要的10！
    值得思考if 以及else的顺序。

---
+++ P5275之迷惑.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int a[n][n];
+	int x=1;
+	int i,j;
+	
+
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			a[i][j]=x;
+			x++;
+		}
+	}
+	
+	
+	for(int t1=0;t1<n;t1++)
+	{
+		for(int t2=0;t2<n;t2++)
+		{
+			if(a[t1][t2]>10){
+				printf("%d");
+			}
+			else{
+				printf("X%d",a[t1][t2]);
+			}
+		}
+		printf("\n");
+	}
+}
